dadddd6fa508e9a23234076aabbd49c98c4555e3
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-02-03 23:43:42 -0300

    Alterado regra do jogo de 10 para 100

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,27 +1,75 @@
-alert("Boas vindas ao jogo do numero secreto");
-//Math.random para gerar numeros aleatorios (0.1234)
-//Math.floor para arredondar o numero gerados para baixo(0.1234 = 1)
-let mutiplicador = 1000;
-let numeroSecreto = Math.floor(Math.random() * mutiplicador) + 1;
-let chute
+let listaDeNumerosSorteados = [];
+let numeroLimite = 3;
+let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-// Enquando o chute for diferente do numero secreto, repete-se o codigo e conta as tentativas.
-console.log(numeroSecreto)
-while (chute != numeroSecreto) {
-  chute = prompt(`Chute um numero de 1 a ${mutiplicador}`);
+function gerarNumeroAleatorio() {
+  let numerEscolhido = parseInt(Math.random() * numeroLimite + 1);
+  let quantidadeDeElementosNaLista = limparCampo.length;
 
-  // se o chute for igual ao numero secreto, executar o alert de acerto, se não executar o alert de erro e repete.
-  if (chute == numeroSecreto) {
-    break;
-  } else if (chute <= numeroSecreto) {
-    alert(`O umero secreto é maior > que ${chute}`);
-  } else {
-    alert(`O umero secreto é menor < que ${chute}`);
+  if (quantidadeDeElementosNaLista == numeroLimite) {
+    listaDeNumerosSorteados = [];
   }
-  tentativas++;
+
+  if (listaDeNumerosSorteados.includes(numerEscolhido)) {
+    return gerarNumeroAleatorio();
+  } else {
+    listaDeNumerosSorteados.push(numerEscolhido);
+    console.log(listaDeNumerosSorteados);
+    return numerEscolhido;
+  }
+}
+
+function exibirNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+  if ('speechSynthesis' in window) {
+    let utterance = new SpeechSynthesisUtterance(texto);
+    utterance.lang = 'pt-BR';
+    utterance.rate = 1.2;
+    window.speechSynthesis.speak(utterance);
+  } else {
+    console.log("Web Speech API não suportada neste navegador.");
+  }
+}
+
+function exibirMensagemInicial() {
+  exibirNaTela('h1', 'JOGO DO NUMERO SECRETO!');
+  exibirNaTela('p', 'Chute um numero entre 1 a 10!');
+}
+exibirMensagemInicial();
+
+function verificarChute() {
+  let chute = document.querySelector('input').value;
+  console.log(numeroSecreto);
+
+
+  if (chute == numeroSecreto) {
+    exibirNaTela('h1', 'Você acertou o numero secreto!');
+    let palavraTentativa = tentativas > 1 ? 'Tentativas' : 'Tentativa';
+    let mensagemTentativas = `Você descobriu o numero secreto que era: ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`;
+    exibirNaTela('p', mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  } else {
+    if (chute > numeroSecreto) {
+      exibirNaTela('p', 'O numero secreto é menor!');
+    } else {
+      exibirNaTela('p', 'O numero secreto é maior!');
+    }
+
+    tentativas++;
+  } limparCampo();
+}
+function limparCampo() {
+  chute = document.querySelector('input');
+  chute.value = '';
+}
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disabled', true);
 
 }
-// Garante a concordancia gramatical sobre tentativa no singular e tentativas no plural
-let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-alert(`Você acertou o numero secreto que era: ${numeroSecreto}, Você acertou com ${tentativas} ${palavraTentativa}.`);
